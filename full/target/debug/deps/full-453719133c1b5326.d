8ed6f5b6c83ca4d5ad040e35141c1badf1e737d4
mika <mika@mikas-Air.mshome.net>
2021-11-11 21:41:52 +0200

    Added a project

---
+++ full/target/debug/deps/full-453719133c1b5326.d
@@ -0,0 +1,5 @@
+/Users/mika/Desktop/code/Repos/rust/full/target/debug/deps/full-453719133c1b5326: src/main.rs
+
+/Users/mika/Desktop/code/Repos/rust/full/target/debug/deps/full-453719133c1b5326.d: src/main.rs
+
+src/main.rs:
---
+++ full/target/debug/full.d
@@ -0,0 +1,1 @@
+/Users/mika/Desktop/code/Repos/rust/full/target/debug/full: /Users/mika/Desktop/code/Repos/rust/full/src/main.rs

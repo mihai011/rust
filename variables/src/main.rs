a00ca0d2de6ede1f9209e185c52c4e32e586446e
mika <mika@mikas-Air.mshome.net>
2021-11-13 18:37:21 +0200

    Added some code for this

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     x = 6;
     println!("The value of x is {}", x);
 
-    let x  = 5;
+    let x = 5;
 
     let x = x + 1;
     let x = x * x;
@@ -19,21 +19,45 @@ fn main() {
     let heart_eyed_cat = "ㇲ";
     println!("{}", heart_eyed_cat);
 
-    let mut tup : (i32, f64, u8) = (23, 23.4, 8);
+    let mut tup: (i32, f64, u8) = (23, 23.4, 8);
     println!("{:?}", tup);
     tup.0 = 34;
     println!("{:?}", tup);
 
-    let a = [1,2,3,4,5];
+    let a = [1, 2, 3, 4, 5];
     let first = a[0];
     let second = a[1];
     println!("{:?}", a);
-    let x =12;
-    x = another_function(x);
+    let x = 12;
+    another_function(x);
     println!("{}", x);
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("Value of y {}", y);
+
+    println!("{}", five());
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
 }
 
-fn another_function(mut x: i32){
+fn another_function(mut x: i32) {
     println!("Another function : {}", x);
     x = x + 1;
+}
+
+fn five() -> i32 {
+    5
 }

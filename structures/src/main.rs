5fe17f04a54b936d8d21b4e7bbf975d79a32e403
mika <mika@mikas-Air.mshome.net>
2021-11-20 20:03:09 +0200

    Added debugger settings

--- structures/src/main.rs
+++ structures/src/main.rs
@@ -6,6 +6,26 @@ struct User{
     active: bool,
 }
 
+#[derive(Debug)]
+struct Rectangle{
+    height: u32,
+    width: u32
+}
+
+impl Rectangle{
+    fn area(&self)->u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool{
+        self.width > other.width && self.width > other.height
+    }
+
+    fn square(size: u32) -> Rectangle{
+        Rectangle { height: size, width: size }
+    }
+}
+
 fn main(){
     let mut user1 = User {
         email: String::from("someone@example.com"),
@@ -21,6 +41,29 @@ fn main(){
     let user2 = build_user(String::from("test2@gmail.com"), String::from("control"));
 
     println!("{}", user2.email);
+
+    let user2=User{
+        username: String::from("trusted username"),
+        //email: String::from("locale_email"),
+        ..user1
+    };
+
+    println!("{}",user2.email);
+
+    let rect1 = Rectangle{height:30,width:50};
+
+    let area = area(&rect1);
+
+    println!("{}", area);
+    println!("{:#?}", rect1);
+    println!("{}",rect1.area());
+
+    let rect2 = Rectangle{height:30,width:50};
+    println!("{}", rect1.can_hold(&rect2));
+}   
+
+fn area( rect: &Rectangle)-> u32{
+    rect.height* rect.width
 }
 
 fn build_user(username: String, email: String) -> User{

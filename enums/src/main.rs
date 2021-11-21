6cda1c8ca39c749366a614e51df068ce2863df04
mika <mika@mikas-Air.mshome.net>
2021-11-21 23:21:06 +0200

    Added some code

---
+++ enums/src/main.rs
@@ -0,0 +1,17 @@
+enum IpAddr{
+    V4(String),
+    V6(String)
+}
+
+enum Message{
+    Quit,
+    Move {x : i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+
+fn main() {
+    let home =IpAddr::V4(String::from("127.0.0.1"));
+
+}

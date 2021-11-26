90870590b0708590aa87b12b27fb62683f85561f
mika <mika@mikas-Air.mshome.net>
2021-11-26 22:14:46 +0200

    Added some breakfast

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -4,13 +4,42 @@ mod front_of_house{
         fn seat_at_table(){}
     }
 
-    mod serving {
+    pub mod serving {
         fn take_order(){}
 
-        fn server_order(){}
+        pub fn serve_order(){ println!("Serving order")}
 
         fn take_payment(){}
+
+        
     }
+}
+
+mod back_of_the_house{
+
+    pub struct Breakfast{
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer_breakfast(toast: &str) -> Breakfast {
+            Breakfast{
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn cook_order(){
+        println!("'Cooking order!");
+    }
+
+    pub fn fix_incorect_order(){
+        cook_order();
+        super::front_of_house::serving::serve_order();
+    }
+    
 }
 
 pub fn eat_at_restaurant(){
@@ -19,14 +48,37 @@ pub fn eat_at_restaurant(){
 
     //relative path
     front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_the_house::Breakfast::summer_breakfast("Rye");
+    // Change our mind about what bread we;'d like
+    println!("I'd like {} toast please", meal.toast);
+
+    let test:String;
+
+    // The next line won't compile if we uncomment it: we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    //meal.seasonal_fruit =  String::from("Blueberries");
 }
 
 
 mod tests{
-    use crate::front_of_house;
+    use crate::{back_of_the_house::{self, Breakfast}, front_of_house};
 
     #[test]
     fn it_works(){
         assert!(front_of_house::hosting::add_to_waitlist() == 4);
+    }
+
+    #[test]
+    fn test_incorrect_order(){
+        back_of_the_house::fix_incorect_order();
+    }
+
+    #[test]
+    fn test_breakfast() {
+        let breakfast: Breakfast = Breakfast::summer_breakfast("toast_with_bacon");
+
+        assert!(breakfast.toast == "toast_with_bacon")
     }
 }
